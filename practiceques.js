0114ed95c93dbec038084a581bc8a96da59a5cac
SuveshBhusal <suvsal8@gmail.com>
2022-08-30 13:57:37 -0400

    practice question html and js
    part 1 of js

--- practiceques.js
+++ practiceques.js
@@ -1,1 +1,11 @@
-// to be added
+function check() {
+    var question1 = document.quiz.jquestion1.value;
+    var correct = 0;
+
+
+    if jquestion1 == "いきました" (
+        correct++;
+
+    )
+    document.getElementById("after_submit").style.visibility = "visible";
+}
